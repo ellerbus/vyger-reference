4e3bcff2523d5859cc387dded77331c973a8fe17
Stu <stu.ellerbusch@gmail.com>
2018-11-21 07:59:21 -0500

    Unit Test Sample

--- vyger-app/src/app/app.component.spec.ts
+++ vyger-app/src/app/app.component.spec.ts
@@ -1,17 +1,42 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { FlashMessageService } from 'src/services/flash-message.service';
+import { Observable } from 'rxjs';
 
-describe('AppComponent', () => {
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [
-                AppComponent
-            ],
-            schemas: [CUSTOM_ELEMENTS_SCHEMA]
-        }).compileComponents();
+describe('AppComponent', () =>
+{
+    class MockRouter
+    {
+        public ne = new NavigationEnd(0, 'http://localhost:4200/login', 'http://localhost:4200/login');
+        public events = new Observable(observer =>
+        {
+            observer.next(this.ne);
+            observer.complete();
+        });
+    }
+
+    let mockRouter: MockRouter;
+    let mockFlashMessageService: jasmine.SpyObj<FlashMessageService>;
+
+    beforeEach(async(() =>
+    {
+        mockRouter = new MockRouter();
+        mockFlashMessageService = jasmine.createSpyObj('FlashMessageService', ['clean']);
+
+        let options = {
+            declarations: [AppComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+            providers: [
+                { provide: FlashMessageService, useValue: mockFlashMessageService },
+                { provide: Router, useValue: mockRouter }
+            ]
+        };
+        TestBed.configureTestingModule(options).compileComponents();
     }));
-    it('should create the app', async(() => {
+    it('should create the app', async(() =>
+    {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
